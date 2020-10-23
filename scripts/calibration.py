3d727c185d3d82da20cf7d03628275c0ab0d92cf
Jobandeep Singh <46861602+jobandeepSingh@users.noreply.github.com>
2020-10-24 01:13:41 +1300

    added tremor data to calibration, and corrected predicting of mock classifier

--- scripts/calibration.py
+++ scripts/calibration.py
@@ -3,29 +3,41 @@ import os
 from glob import glob
 
 import matplotlib.pyplot as plt
+from whakaari import TremorData, ForecastModel
+from datetime import timedelta, datetime
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.calibration import CalibratedClassifierCV
+from time import time
 
 
 # new class to mock the forecast_model class
 class MockForecastModel(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, model_path: str = "models"):
+    def __init__(self, model_path: str):
         """
 
-        :param model_path: path to model decision trees, relative to root of repo.
-            Defaults to models directory.
+        :param model_path: path to model decision trees.
         """
         self.model_path = model_path
         self.trees = []
+        self.feats = []
         self._read_trees()
 
     def _read_trees(self):
-        tree_files = glob(f"{path}\\*.pkl")
+        # read in the trees and save to self.trees
+        tree_files = glob(f"{self.model_path}\\*.pkl")
         for tree in tree_files:
             self.trees.append(joblib.load(tree))
+
+        # read in the feature files for each tree
+        tree_feat_files = glob(f"{self.model_path}\\[0-9]*.fts")
+        for feat_file in tree_feat_files:
+            f = open(feat_file, 'r')
+            feats = list(map(lambda x: x.strip().split(' ', 1)[1], f.readlines()))
+            f.close()
+            self.feats.append(feats)
 
     def fit(self, X, y):
         """
@@ -53,10 +65,12 @@ class MockForecastModel(BaseEstimator, ClassifierMixin):
 
     def predict_proba(self, X):
         # initialise probability matrix, shape = nx2
-        y_proba = np.zeros((len(self.trees), 2))
+        y_proba = np.zeros((X.shape[0], 2))
 
-        for tree in self.trees:
-            pred = tree.predict(X)
+        for idx, tree in enumerate(self.trees):
+            # use features from feature files when predicting so they match the features used for training
+            pred = tree.predict(X[self.feats[idx]])
+
             # turn the predictions into indices 0 and 1 by converting them to integer
             pred = list(map(int, np.round(pred)))
             # add a count to the classification made for each observation
@@ -69,20 +83,46 @@ class MockForecastModel(BaseEstimator, ClassifierMixin):
         return y_proba
 
 
-if __name__ == '__main__':
-    os.chdir('..')  # set working directory to root
+def calibration(download_data=False):
+    # constants
+    month = timedelta(days=365.25 / 12)
 
-    # path to model
-    path = "models\\fm_2.00wndw_0.75ovlp_2.00lkfd_dsar-hf-mf-rsam"
+    td = TremorData()
+    # QUESTION is this needed???
+    if download_data:
+        for te in td.tes:
+            td.update(ti=te-month, tf=te+month)
 
-    classifier = MockForecastModel(path)
+    # construct model object
+    data_streams = ['rsam', 'mf', 'hf', 'dsar']
+    fm = ForecastModel(ti='2011-01-01', tf='2020-01-01', window=2., overlap=0.75,
+                       look_forward=2., data_streams=data_streams, root='calibration_forecast_model')
 
-    # TODO need to read in actual data here
-    df = pd.DataFrame(np.random.randint(0, 100, size=(100, 20)), columns=list('ABCDEFGHIJKLMNOPRSTU'))
-    classifier.predict_proba(df)
+    # columns to manually drop from feature matrix because they are highly correlated to other
+    # linear regressors
+    drop_features = ['linear_trend_timewise', 'agg_linear_trend']
+
+    # set the available CPUs higher or lower as appropriate
+    n_jobs = 6
+
+    # train the model, excluding 2019 eruption
+    # note: building the feature matrix may take several hours, but only has to be done once
+    # and will intermittantly save progress in ../features/
+    # trained scikit-learn models will be saved to ../models/*root*/
+    te = td.tes[-1]
+    fm.train(ti='2011-01-01', tf='2020-01-01', drop_features=drop_features, retrain=True,
+             exclude_dates=[[te - month, te + month], ], n_jobs=n_jobs)
+
+    classifier = MockForecastModel(fm.modeldir)
+
+    # get feature and labels for eruption not used in training
+    X, y = fm._extract_features(ti=te - month, tf=te + month)
 
     cccv = CalibratedClassifierCV(classifier, cv='prefit')
-    cccv.fit()  # TODO add out of sample X and y as input
+    cccv.fit(X, y['label'])
     # TODO your_cccv.fit(X_validation, y_validation) <- This validation data is used solely for calibration purposes.
 
-    pass
+
+if __name__ == '__main__':
+    os.chdir('..')  # set working directory to root
+    calibration()
