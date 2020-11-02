66d18e0ec791cdd0b4714ffd68be0ac03eef6d87
Jobandeep Singh <46861602+jobandeepSingh@users.noreply.github.com>
2020-11-02 22:13:10 +1300

    removed comments and tied up imports

--- scripts/calibration.py
+++ scripts/calibration.py
@@ -4,7 +4,7 @@ from glob import glob
 
 import matplotlib.pyplot as plt
 from whakaari import TremorData, ForecastModel
-from datetime import timedelta, datetime
+from datetime import timedelta
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
@@ -111,7 +111,6 @@ def calibration(download_data=False, plots=True):
     month = timedelta(days=365.25 / 12)
 
     td = TremorData()
-    # QUESTION is this needed???
     if download_data:
         for te in td.tes:
             td.update(ti=te-month, tf=te+month)
@@ -154,7 +153,6 @@ def calibration(download_data=False, plots=True):
         plt.axvline(0.8, color='pink', linewidth=5, zorder=1)
         plt.scatter(classifier.predict_proba(X)[:, 1], calibrated_classifier.predict_proba(X)[:, 1],
                     c='orange', s=60, zorder=2)
-        # plt.axhline(0.086, color='black', xmax=0.82, linewidth=2, zorder=1, ls='--')
 
         plt.xlabel("Threshold for predicting eruption", fontsize=25)
         plt.ylabel("Probability of eruption", fontsize=25)
